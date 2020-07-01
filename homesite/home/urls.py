e4e299a12c1e27f24405ade2570261dc9b137409
Nathaniel Cherian <ngcherian2014@gmail.com>
2020-07-01 12:12:38 -0700

    contact us page

--- homesite/home/urls.py
+++ homesite/home/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import home_page
+from .views import home_page, about_page
 
 urlpatterns = [
     path('', home_page, name='home_page'),
+    path('about/', about_page, name='about_page'),
+
 ]
--- homesite/home/views.py
+++ homesite/home/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def home_page(request):
     return render(request, 'home/home.html')
+
+def about_page(request):
+    return render(request, 'home/about.html')
