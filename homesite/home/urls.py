7a8554f1c4f7ce09b6be58610d32984461e83ed6
Nathaniel Cherian <ngcherian2014@gmail.com>
2020-07-09 15:03:06 -0700

    fixing

--- homesite/home/urls.py
+++ homesite/home/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from .views import home_page, about_page, product_page, donate_page
+from .views import home_page, about_page, product_page, donate_page, courses_page
 
 urlpatterns = [
     path('', home_page, name='home_page'),
