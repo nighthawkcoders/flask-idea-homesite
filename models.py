d65690df87c42f8696f4803c3c6643a79004a730
John Mortensen <jmortensen@powayusd.com>
2020-11-26 14:50:04 -0800

    Using Jinja and Includes

--- models.py
+++ models.py
@@ -1,3 +1,11 @@
-from django.db import models
 
-# Create your models here.
+
+def ap():
+    greeting = "Hey, Hey, Hey!"
+    name = "Repl"
+    doa = "October 30"
+    job = "Runtime Link"
+    embed = "https://Python-Web-Portfolio-Series.jmort1021.repl.co"
+    info = {"greeting": greeting, "name": name, "doa": doa, "job": job, "embed": embed}
+    return infofrom django.db import models
+
