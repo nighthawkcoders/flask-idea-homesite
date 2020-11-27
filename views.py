a5bda47d2b89ac8941561c9f7b64bb1807396cc7
John Mortensen <jmortensen@powayusd.com>
2020-11-26 17:28:04 -0800

    organizing files

--- views.py
+++ views.py
@@ -5,40 +5,40 @@ from models import java_ap, java_hello, java_mvc, python_ap, python_flask, pytho
 
 @app.route('/')
 def index():
-    return render_template("home.html")
+    return render_template("homesite/home.html")
 
 @app.route('/java')
 def java():
-    return render_template("java.html")
+    return render_template("homesite/java.html")
 
 @app.route('/java/hello')
 def javahello():
-    return render_template("project.html", data=java_hello())
+    return render_template("homesite/project.html", data=java_hello())
 
 @app.route('/java/mvc')
 def javamvc():
-    return render_template("project.html", data=java_mvc())
+    return render_template("homesite/project.html", data=java_mvc())
 
 @app.route('/java/ap')
 def javaap():
-    return render_template("project.html", data=java_ap())
+    return render_template("homesite/project.html", data=java_ap())
 
 @app.route('/python')
 def python():
-    return render_template("python.html")
+    return render_template("homesite/python.html")
 
 @app.route('/python/hello')
 def pythonhello():
-    return render_template("project.html", data=python_hello())
+    return render_template("homesite/project.html", data=python_hello())
 
 @app.route('/python/flask')
 def pythonflask():
-    return render_template("project.html", data=python_flask())
+    return render_template("homesite/project.html", data=python_flask())
 
 @app.route('/python/ap')
 def pythonap():
-    return render_template("project.html", data=python_ap())
+    return render_template("homesite/project.html", data=python_ap())
 
 @app.route('/about')
 def about():
-    return render_template("about.html")
+    return render_template("homesite/about.html")
