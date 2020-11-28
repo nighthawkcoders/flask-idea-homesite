4a9f4f615e00f5e292a1f67345975baaa565cdc7
John Mortensen <jmortensen@powayusd.com>
2020-11-28 06:58:45 -0800

    Stubs for menus

--- views.py
+++ views.py
@@ -4,9 +4,13 @@ from __init__ import app
 from models import java_ap, java_hello, java_mvc, java_event, java_study, java_projects
 from models import python_hello, python_ap, python_flask, python_study, python_projects
 
+"""Dropdown Section"""
 # This table is used to inform HTML of primary menu items and routes
 menus = [{"title": 'Java', "route": '.java'},
          {"title": 'Python', "route": '.python'},
+         {"title": 'Git', "route": '.git'},
+         {"title": 'Pi', "route": '.pi'},
+         {"title": 'Scrum', "route": '.scrum'},
          {"title": 'About', "route": '.about'}]
 
 
@@ -18,6 +22,35 @@ def index():
 @app.route('/java')
 def java():
     return render_template("homesite/landing.html", heading="Java", menus=menus, projects=java_projects())
+
+
+@app.route('/python')
+def python():
+    return render_template("homesite/landing.html", heading="Python", menus=menus, projects=python_projects())
+
+
+
+@app.route('/git')
+def git():
+    return render_template("homesite/git.html", menus=menus)
+
+
+@app.route('/pi')
+def pi():
+    return render_template("homesite/pi.html", menus=menus)
+
+
+@app.route('/scrum')
+def scrum():
+    return render_template("homesite/scrum.html", menus=menus)
+
+
+@app.route('/about')
+def about():
+    return render_template("homesite/about.html", menus=menus)
+
+
+"""Java Section"""
 
 
 @app.route('/java/hello')
@@ -39,14 +72,13 @@ def javaap():
 def javaevent():
     return render_template("homesite/project.html", menus=menus, data=java_event())
 
+
 @app.route('/java/study')
 def javastudy():
     return render_template("homesite/project.html", menus=menus, data=java_study())
 
 
-@app.route('/python')
-def python():
-    return render_template("homesite/landing.html", heading="Python", menus=menus, projects=python_projects())
+"""Python Section"""
 
 
 @app.route('/python/hello')
@@ -63,10 +95,7 @@ def pythonflask():
 def pythonap():
     return render_template("homesite/project.html", menus=menus, data=python_ap())
 
+
 @app.route('/python/study')
 def pythonstudy():
     return render_template("homesite/project.html", menus=menus, data=python_study())
-
-@app.route('/about')
-def about():
-    return render_template("homesite/about.html", menus=menus)
