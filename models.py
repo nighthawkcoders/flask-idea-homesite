2324ed0eff4258f30123b329007ffebbd670571b
John Mortensen <jmortensen@powayusd.com>
2020-11-27 17:17:16 -0800

    Templates moved to homesite directory

--- models.py
+++ models.py
@@ -7,7 +7,7 @@ def java_ap():
     group = "java"
     tag = "ap"
     title = "AP study"
-    timeline = "https://padlet.com/embed/iji39ih2mzwa"
+    timeline = "https://padlet.com/jmortensen7/APCentralCSA_p1"
     data = {"group": group, "tag": tag, "title": title, "timeline": timeline}
     return data
 
@@ -16,7 +16,7 @@ def java_hello():
     group = "java"
     tag = "hello"
     title = "Hello series"
-    timeline = "https://padlet.com/embed/lslb7s7y06sf"
+    timeline = "https://padlet.com/jmortensen7/csatime"
     repo = "https://gh-card.dev/repos/nighthawkcoders/javahello.svg?link_target=_blank"
     repo_description = "Hello, World! and a variety of classroom and technical introductions"
     repl = "https://repl.it/@jmort1021/Java-Hello-Series?lite=true"
@@ -29,7 +29,7 @@ def java_mvc():
     group = "java"
     tag = "mvc"
     title = "MVC ideas"
-    timeline = "https://padlet.com/embed/iji39ih2mzwa"
+    timeline = "https://padlet.com/jmortensen7/csatime1_2"
     repo = "https://gh-card.dev/repos/nighthawkcoders/mvc-idea.svg?link_target=_blank"
     repo_description = "Ideas for coding in the Model View Control (MVC) style."
     data = {"group": group, "tag": tag, "title": title, "timeline": timeline, "repo": repo,
