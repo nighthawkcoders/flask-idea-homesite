b76e37248899238b66469359b153661193763384
John Mortensen <jmortensen@powayusd.com>
2020-12-06 20:40:51 -0800

    Support counts correctly

--- __init__.py
+++ __init__.py
@@ -2,11 +2,11 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 app = Flask(__name__)
-table = 'sqlite:///models/myDB.db'
+dbURI = 'sqlite:///models/myDB.db'
 
 """ database setup to support db examples """
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = table
+app.config['SQLALCHEMY_DATABASE_URI'] = dbURI
 db = SQLAlchemy(app)
 
 """ blue print to move db to its own folder """
--- pythondb/view.py
+++ pythondb/view.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 from pythondb import pythondb_bp
 from pythondb.model import Users, Emails, PhoneNumbers
 from models import menus
-from __init__ import table
+from __init__ import dbURI
 
 
 # Declare your Users table
@@ -52,7 +52,7 @@ def databases():
 @pythondb_bp.route('/create/', methods=["POST"])
 def create():
     if request.form:
-        engine = create_engine(table, echo=True)  # relative path within project
+        engine = create_engine(dbURI, echo=True)  # relative path within project
         Session = sessionmaker(bind=engine)
         session = Session()
         user = Users(username=request.form.get("username"), password=request.form.get("password"))
@@ -95,39 +95,24 @@ def read():
 @pythondb_bp.route('/update/', methods=["POST"])
 def update():
     if request.form:
-        engine = create_engine(table, echo=True)  # relative path within project
+        userid = request.form.get("ID")
+        print(userid)
+        engine = create_engine(dbURI, echo=True)  # relative path within project
         Session = sessionmaker(bind=engine)
         session = Session()
-        userid = request.form.get("ID")
         session.query(Emails).filter_by(UserID=userid).update({Emails.email_address: request.form.get("email")})
         session.commit()
         session.query(PhoneNumbers).filter_by(UserID=userid).update(
             {PhoneNumbers.phone_number: request.form.get("phone_number")})
         session.commit()
-        users = Users.query.all()
-        records = []
-        for user in users:
-            user_dict = {'id': user.UserID, 'name': user.username, 'password': user.password}
-            # associate emails with user
-            emails = Emails.query.all()
-            for email in emails:
-                if user_dict['id'] == email.UserID:
-                    user_dict['emails'] = email.email_address
-            # associate phone numbers with user
-            phone_numbers = PhoneNumbers.query.all()
-            for pn in phone_numbers:
-                if user_dict['id'] == pn.UserID:
-                    user_dict['phone_numbers'] = pn.phone_number
-            # add record to list
-            records.append(user_dict)
-    return render_template("pythondb/index.html", table=records, menus=menus)
+    return redirect(url_for('pythondb_bp.databases'))
 
 
 # CRUD delete
 @pythondb_bp.route('/delete/', methods=["POST"])
 def delete():
     if request.form:
-        engine = create_engine(table, echo=True)  # relative path within project
+        engine = create_engine(dbURI, echo=True)  # relative path within project
         Session = sessionmaker(bind=engine)
         session = Session()
         # userid = request.form.get("ID")
@@ -137,26 +122,10 @@ def delete():
         # phone = PhoneNumbers.query.filter_by(UserID=userid)
         # phone.phone_number = request.form.get("phone_number")
         session.commit()
-        users = Users.query.all()
-        records = []
-        for user in users:
-            user_dict = {'id': user.UserID, 'name': user.username, 'password': user.password}
-            # associate emails with user
-            emails = Emails.query.all()
-            for email in emails:
-                if user_dict['id'] == email.UserID:
-                    user_dict['emails'] = email.email_address
-            # associate phone numbers with user
-            phone_numbers = PhoneNumbers.query.all()
-            for pn in phone_numbers:
-                if user_dict['id'] == pn.UserID:
-                    user_dict['phone_numbers'] = pn.phone_number
-            # add record to list
-            records.append(user_dict)
+    return redirect(url_for('pythondb_bp.databases'))
 
-    return render_template("pythondb/index.html", table=records, menu=menus)
 
-# if email url, show the email table
+# if email url, show the email table only
 @pythondb_bp.route('/emails/')
 def emails():
     # fill the table with emails only
@@ -170,7 +139,7 @@ def emails():
     return render_template("pythondb/index.html", table=records, menu=menus)
 
 
-# if phones url, shjow phones table
+# if phones url, show phones table only
 @pythondb_bp.route('/phones/')
 def phones():
     # fill the table with phone numbers only
