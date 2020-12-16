53766c1c51c82f4da144839884c3c7fb6bc10682
iniyaa <iniyaam21@gmail.com>
2020-12-16 12:52:03 -0800

    updated to new format

--- __init__.py
+++
@@ -1,3 +0,0 @@
-"""_init_.py is used to define app and all blueprints"""
-from flask import Flask
-app = Flask(__name__)
--- wsgi.py
+++
@@ -1,7 +0,0 @@
-"""Driver for Web server"""
-
-from views import app
-
-if __name__ == "__main__":
-    # runs the application on the repl development server
-    app.run(debug=True, host='127.0.0.1', port='8080')
