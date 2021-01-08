08919eaee36a28f052890f86963d922f754885b6
iniyaa <iniyaam21@gmail.com>
2021-01-07 20:52:58 -0800

    files and routes

--- main.py
+++ main.py
@@ -10,40 +10,40 @@ app = Flask(__name__)
 def home_route():
   return render_template("home.html", projects=projects.setup())
 
-#connects /hello path of server to render comediandecades.html
-@app.route('/comediandecades/')
+#connects /hello path of server to render NY.html
+@app.route('/NY/')
 def comediandecades_route():
-  return render_template("comediandecades.html", projects=projects.setup())
+  return render_template("NY.html", projects=projects.setup())
 
-#connects /historyofcomedy path of server to render historyofcomedy.html
-@app.route('/historyofcomedy/')
+#connects /historyofcomedy path of server to render NJ.html
+@app.route('/NJ/')
 def historyofcomedy_route():
-  return render_template("historyofcomedy.html", projects=projects.setup())
+  return render_template("NJ.html", projects=projects.setup())
 
-#connects /knock path of server to render knock.html
-@app.route('/knock')
+#connects /knock path of server to render FL.html
+@app.route('/FL')
 def knock_route():
-  return render_template("knock.html", projects=projects.setup())
+  return render_template("FL.html", projects=projects.setup())
   
-#connects /iniyaa path of server to render iniyaa.html
-@app.route('/iniyaa')
+#connects /iniyaa path of server to render MA.html
+@app.route('/MA')
 def iniyaa_route():
-  return render_template("iniyaa.html", projects=projects.setup())
+  return render_template("MA.html", projects=projects.setup())
 
-#connects /ketki path of server to render ketki.html
-@app.route('/ketki')
+#connects /ketki path of server to render OR.html
+@app.route('/OR')
 def ketki_route():
-  return render_template("ketki.html", projects=projects.setup())
+  return render_template("OR.html", projects=projects.setup())
 
-#connects /ava path of server to render ava.html
-@app.route('/ava')
+#connects /ava path of server to render CA.html
+@app.route('/CA')
 def ava_route():
-  return render_template("ava.html", projects=projects.setup())
+  return render_template("CA.html", projects=projects.setup())
 
-#connects /lucas path of server to render lucas.html
-@app.route('/lucas')
+#connects /lucas path of server to render TX.html
+@app.route('/TX')
 def lucas_route():
-  return render_template("lucas.html", projects=projects.setup())
+  return render_template("TX.html", projects=projects.setup())
 
 #connects /submit path of server to render submit.html
 @app.route('/submit')
